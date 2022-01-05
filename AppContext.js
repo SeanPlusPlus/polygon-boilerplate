8a616435c27591656e25d5b78bbdb6a456253532
Sean Stephenson <Sean.M.Stephenson@disney.com>
2022-01-05 15:23:16 -0800

    dotenv on api

--- AppContext.js
+++ AppContext.js
@@ -1,13 +1,15 @@
 import { createContext, useContext } from "react";
+
 // import { network, config } from '@/config';
 // import NFT from "../hardhat/artifacts/hardhat/contracts/NFT.sol/NFT.json";
 // import NFT from "@/lib/NFT.json";
+
 
 export const defaultValues = {
   // network,
   // config,
   // NFT,
-  CONTRACT_ADDRESS: "CONTRACT_ADDRESS",
+  CONTRACT_ADDRESS: 'hello world',
 };
 
 const AppContext = createContext(defaultValues);
