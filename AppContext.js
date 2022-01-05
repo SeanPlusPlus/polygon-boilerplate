5f7ebe3cdf122b4b76b3fffd40174e908612b5a9
Sean Stephenson <Sean.M.Stephenson@disney.com>
2022-01-05 15:40:23 -0800

    use axios to fetch data

--- AppContext.js
+++ AppContext.js
@@ -1,9 +1,19 @@
 import { createContext, useContext } from "react";
+import axios from "axios";
+
 
 // import { network, config } from '@/config';
 // import NFT from "../hardhat/artifacts/hardhat/contracts/NFT.sol/NFT.json";
 // import NFT from "@/lib/NFT.json";
 
+const fetchData = async (week, year) => {
+  const api = await axios.get(
+      `/api/data`
+  );
+  console.log("api", api.data);
+};
+
+fetchData();
 
 export const defaultValues = {
   // network,
