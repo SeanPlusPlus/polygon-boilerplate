9df53777b17b5e5819668d8be3058237d156f384
Sean Stephenson <Sean.M.Stephenson@disney.com>
2022-01-06 15:05:21 -0800

    add json

--- context/AppContext.js
+++ context/AppContext.js
@@ -2,9 +2,11 @@ import { createContext, useContext } from "react";
 
 // import NFT from "../hardhat/artifacts/hardhat/contracts/NFT.sol/NFT.json";
 
+const WAVE_PORTAL_ADDRESS = '0xf7BFa193035855DF416AeC3BAd97c6672142fC6C'
+
 export const defaultValues = {
   // NFT,
-  CONTRACT_ADDRESS: '0xb477ccf4f9411AE914CE91Dc0a87AFb08316f956',
+  CONTRACT_ADDRESS: WAVE_PORTAL_ADDRESS,
 };
 
 const AppContext = createContext(defaultValues);
