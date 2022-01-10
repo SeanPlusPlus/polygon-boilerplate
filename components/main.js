40a793cd07009a4e4aed72c34351adda4324a1e8
Sean Stephenson <Sean.M.Stephenson@disney.com>
2022-01-10 10:32:39 -0800

    update msg

--- components/main.js
+++ components/main.js
@@ -150,7 +150,7 @@ export default function Main() {
 
       <Nav address={address} />
 
-      <main className="flex mb-4">
+      <main className="flex m-2">
         <div className="lg:w-3/12 m-1 hidden lg:block">
           <div className="grid grid-cols-1 gap-6 lg:p-10 lg:bg-base-200 rounded-box mb-5">
             <div className="card shadow-lg compact side bg-base-100 mb-1">
@@ -158,9 +158,11 @@ export default function Main() {
                 <h2 className="my-4 text-4xl font-bold card-title">
                   Hello <span role="img" aria-label="wave">👋</span>
                 </h2>
-                <p className="pb-5">
-                  Welcome to my web3 boilerplate project; feel free to post a message to the board and say hi!
-                </p>
+                <div className="bio">
+                  <p className="pb-5">
+                    Hiya there! I'm <a className="link link-secondary" href="https://twitter.com/seanplusplus" target="_blank" rel="noopener noreferrer">Sean</a>, and I'm a Sr Software Engineer at Disney digital media. This is a prototype I am <a className="link link-secondary" href="https://github.com/SeanPlusPlus/waveportal-starter-project" target="_blank" rel="noopener noreferrer">hacking together</a> based on <a className="link link-secondary" href="https://app.buildspace.so/projects/CO02cf0f1c-f996-4f50-9669-cf945ca3fb0b/lessons/LEe9f04c2e-fe9c-4e87-81b2-efb677a1720c" target="_blank" rel="noopener noreferrer">this tutorial</a>. Connect your Ethereum wallet (make sure you're on the Rinkeby test network), craft a message, and wave at me! To start, you probably should <a className="link link-secondary" href="https://web3hackathon.vercel.app/how-to" target="_blank" rel="noopener noreferrer">follow this</a> (you can go and mint your very own 🏈 NFT there while you're at it).
+                  </p>
+                </div>
               </div>
             </div>
           </div>
--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,22 @@ export default function App() {
 
   if (!address) {
     return (
-      <div className="flex h-screen">
-        <div className="m-auto">
+      <div className="flex h-screen text-center">
+        <div className="m-auto w-2/3">
+          <div className="grid grid-cols-1 lg:p-10 lg:bg-base-200 rounded-box mb-5">
+            <div className="card shadow-lg compact side bg-base-100 mb-1">
+              <div className="card-body">
+                <h2 className="my-4 text-4xl font-bold card-title">
+                  Hello <span role="img" aria-label="wave">👋</span>
+                </h2>
+                <div className="bio">
+                  <p className="pb-5">
+                    Hiya there! I'm <a className="link link-secondary" href="https://twitter.com/seanplusplus" target="_blank" rel="noopener noreferrer">Sean</a>, and I'm a Sr Software Engineer at Disney digital media. This is a prototype I am <a className="link link-secondary" href="https://github.com/SeanPlusPlus/waveportal-starter-project" target="_blank" rel="noopener noreferrer">hacking together</a> based on <a className="link link-secondary" href="https://app.buildspace.so/projects/CO02cf0f1c-f996-4f50-9669-cf945ca3fb0b/lessons/LEe9f04c2e-fe9c-4e87-81b2-efb677a1720c" target="_blank" rel="noopener noreferrer">this tutorial</a>. Connect your Ethereum wallet (make sure you're on the Rinkeby test network), craft a message, and wave at me! To start, you probably should <a className="link link-secondary" href="https://web3hackathon.vercel.app/how-to" target="_blank" rel="noopener noreferrer">follow this</a> (you can go and mint your very own 🏈 NFT there while you're at it).
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
           <button onClick={() => connectWallet("injected")} className="btn btn-secondary">
             Connect your wallet
           </button>
